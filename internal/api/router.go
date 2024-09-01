cd777db5d0c7adb50d3f22d48203c90ee5e44680
CP Payne <charlethical1@gmail.com>
2024-09-01 10:50:46 +0200

    changed cors middleware (chi) to manual

--- internal/api/router.go
+++ internal/api/router.go
@@ -8,22 +8,23 @@ import (
 	"github.com/CP-Payne/ecomstore/internal/service"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
+
+	cmid "github.com/CP-Payne/ecomstore/internal/api/middleware"
 )
 
 func SetupRouter(db *database.Queries) http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowedMethods:   []string{"GET", "PUT", "POST", "DELETE", "OPTION"},
-		AllowedHeaders:   []string{"*"},
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
+	// r.Use(cors.Handler(cors.Options{
+	// 	AllowedOrigins:   []string{"*"},
+	// 	AllowedMethods:   []string{"GET", "PUT", "POST", "DELETE", "OPTION"},
+	// 	AllowedHeaders:   []string{"*"},
+	// 	AllowCredentials: true,
+	// 	MaxAge:           300, // Maximum value not ignored by any of major browsers
+	// }))
 
-	// r.Use(cmid.CorsMiddleware)
+	r.Use(cmid.CorsMiddleware)
 
 	userSrv := service.NewUserService(db)
 	productSrv := service.NewProductService(db)
