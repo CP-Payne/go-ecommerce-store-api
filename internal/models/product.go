3cfef6cd3b4083ede9fe6ae78e66857ad5dbeb3c
CP Payne <charlethical1@gmail.com>
2024-09-02 07:55:08 +0200

    Update get product endpoint to return specifications and variants along with other data

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"encoding/json"
 	"strconv"
 	"time"
 
@@ -11,19 +12,21 @@ import (
 )
 
 type Product struct {
-	ID           uuid.UUID `json:"id"`
-	Name         string    `json:"name"`
-	Description  string    `json:"description"`
-	Price        float32   `json:"price"`
-	Brand        string    `json:"brand"`
-	Sku          string    `json:"sku"`
-	Stock        int       `json:"stock"`
-	CategoryID   uuid.UUID `json:"categoryId"`
-	ImageURL     string    `json:"imageUrl"`
-	ThumbnailURL string    `json:"thumbnailUrl"`
-	IsActive     bool      `json:"isActive"`
-	CreatedAt    time.Time `json:"createdAt,omitempty"`
-	UpdatedAt    time.Time `json:"updatedAt,omitempty"`
+	ID             uuid.UUID       `json:"id"`
+	Name           string          `json:"name"`
+	Description    string          `json:"description"`
+	Price          float32         `json:"price"`
+	Brand          string          `json:"brand"`
+	Sku            string          `json:"sku"`
+	Stock          int             `json:"stock"`
+	CategoryID     uuid.UUID       `json:"categoryId"`
+	ImageURL       string          `json:"imageUrl"`
+	ThumbnailURL   string          `json:"thumbnailUrl"`
+	Specifications json.RawMessage `json:"specifications"`
+	Variants       json.RawMessage `json:"variants"`
+	IsActive       bool            `json:"isActive"`
+	CreatedAt      time.Time       `json:"createdAt,omitempty"`
+	UpdatedAt      time.Time       `json:"updatedAt,omitempty"`
 }
 
 // Database Product to product mappings
@@ -35,19 +38,20 @@ func DatabaseProductToProduct(product database.Product) Product {
 	}
 
 	return Product{
-		ID:           product.ID,
-		Name:         product.Name,
-		Description:  NullStringToString(product.Description),
-		Price:        float32(price),
-		Brand:        NullStringToString(product.Brand),
-		Sku:          product.Sku,
-		Stock:        int(product.StockQuantity),
-		CategoryID:   product.ID,
-		ImageURL:     NullStringToString(product.ImageUrl),
-		ThumbnailURL: NullStringToString(product.ThumbnailUrl),
-
-		IsActive:  product.IsActive,
-		CreatedAt: product.CreatedAt,
-		UpdatedAt: product.UpdatedAt,
+		ID:             product.ID,
+		Name:           product.Name,
+		Description:    NullStringToString(product.Description),
+		Price:          float32(price),
+		Brand:          NullStringToString(product.Brand),
+		Sku:            product.Sku,
+		Stock:          int(product.StockQuantity),
+		CategoryID:     product.ID,
+		ImageURL:       NullStringToString(product.ImageUrl),
+		ThumbnailURL:   NullStringToString(product.ThumbnailUrl),
+		Specifications: NullRawMessageToRawMessage(product.Specifications),
+		Variants:       NullRawMessageToRawMessage(product.Variants),
+		IsActive:       product.IsActive,
+		CreatedAt:      product.CreatedAt,
+		UpdatedAt:      product.UpdatedAt,
 	}
 }
--- internal/models/shared.go
+++ internal/models/shared.go
@@ -1,10 +1,22 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"encoding/json"
+
+	"github.com/sqlc-dev/pqtype"
+)
 
 func NullStringToString(str sql.NullString) string {
 	if !str.Valid {
 		return ""
 	}
 	return str.String
+}
+
+func NullRawMessageToRawMessage(rawMessage pqtype.NullRawMessage) json.RawMessage {
+	if !rawMessage.Valid {
+		return json.RawMessage{}
+	}
+	return rawMessage.RawMessage
 }
