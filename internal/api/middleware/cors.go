cf2a3c56bce1091f1178c166da85857606c8210e
Charles <charlethical1@gmail.com>
2024-09-04 07:56:24 +0200

    Updated CORS policy

--- internal/api/middleware/cors.go
+++ internal/api/middleware/cors.go
@@ -4,9 +4,9 @@ import "net/http"
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "cmid *")
+		w.Header().Set("Access-Control-Allow-Headers", "cmid *, Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
